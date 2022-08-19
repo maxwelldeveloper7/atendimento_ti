7ff14f19ba004235937bddf7b95b25d0e2190cfe
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-08-19 16:00:07 -0300

    tela inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+titulo = "PREFEITURA MUNICIPAL DE SAÚDE - CONTROLE DE ATENDIMENTOS"
+subtitulo = "INFRAESTRUTURA DE TI"
+
+print(titulo)
+print(len(titulo))
+print("")
+
+
+
+
+
+if (__name__=="__main__"):
+    print('fim')
