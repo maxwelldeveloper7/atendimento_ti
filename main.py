4d31fc1adc254d5d401c998d14225de098e2ef4c
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-08-22 10:50:21 -0300

    refatorando com novos métodos

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-titulo = "PREFEITURA MUNICIPAL DE SAÚDE - CONTROLE DE ATENDIMENTOS"
+import os
+os.system('clear')
+
+def limpar_console():
+    command = 'clear'
+    if os.name in ('nt','dos'):
+        command = 'cls'
+    os.system(command)
+
+
+def escreve_titulo(titulo, subtitulo):
+    
+    largura_titulo = int(len(titulo))#pega a largura do titulo
+    largura_subtitulo = int(len(subtitulo))#pega a largura do subtitulo
+    centro_titulo = int(largura_titulo/2)#calcula o centro do titulo
+    centro_subtitulo = int(largura_subtitulo/2)#calcula o centro do subtitulo
+
+    print(titulo)
+    print((centro_titulo - centro_subtitulo - 2)*" ",subtitulo)#posiciona o subtitulo ao centro do titulo
+
+
+titulo = "PREFEITURA MUNICIPAL DE NANUQUE - CONTROLE DE ATENDIMENTOS"
 subtitulo = "INFRAESTRUTURA DE TI"
 
-print(titulo)
-print(len(titulo))
-print("")
-
-
+limpar_console()
+escreve_titulo(titulo, subtitulo)
 
 
 
