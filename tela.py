607b3eec06312fcde6b5fc8509644bf08720bf0d
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-09-19 15:38:52 -0300

    acolorindo a tela

--- tela.py
+++ tela.py
@@ -15,7 +15,11 @@ def escreve_titulo(titulo, subtitulo):
     centro_titulo = int(largura_titulo/2)#calcula o centro do titulo
     centro_subtitulo = int(largura_subtitulo/2)#calcula o centro do subtitulo
 
-    print("*" * (largura_titulo))
-    print("*", titulo, "*")
-    print("*", (centro_titulo - centro_subtitulo - 3) * " ", subtitulo, " " * (centro_titulo - centro_subtitulo -3), "*")#posiciona o subtitulo ao centro do titulo
-    print("*" * (largura_titulo))
+    pc = "\u001b[32m\U0001f5a5\u001b[m"# emoji de um pc
+
+    print(pc * (largura_titulo))#desenha borda superior
+    print(pc, titulo, pc)#desenha titulo e bordas laterais
+    print(pc, (centro_titulo - centro_subtitulo - 3) * " ", subtitulo, " " * (centro_titulo - centro_subtitulo -3), pc)#posiciona o subtitulo ao centro do titulo e desenha bordas laterais
+    print(pc * (largura_titulo),"\n")#desenha borda inferior
+
+    #print("\u001b[32m\U0001f5a5\u001b[m")
